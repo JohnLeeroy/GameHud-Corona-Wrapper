38a60e9bf94db52821d3302d10afb9aee84ae501
JohnLeeroy <johnliglobal@gmail.com>
2012-12-08 17:19:55 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -12,13 +12,12 @@ local ghAnalytics = require("GHAnalytics")
 local function main()
 ghAnalytics.Init("playerID", "deviceID");
 ghAnalytics.StartSession();
---
 
---
+--Sends off events every 5 seconds
+ghAnalytics.SetSendQueueTimer(5000)
+
 local params = {StringProperty="ThisIsAString", NumberProperty=1337}
 ghAnalytics.SendEvent("EventName", params)
-
-ghAnalytics.SetSendQueueTimer(5000)
 
 local playerData = {}
 playerData.age = 21;
